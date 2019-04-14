f896a72933a01da81d0a13eada9655e6de59b36c
Diego Fabricio <difagume@gmail.com>
2019-04-14 12:51:39 -0500

    agrego server.js para deployar a Heroku

---
+++ server.js
@@ -0,0 +1,20 @@
+/**
+ * Archivo creado únicamente para deployar a Heroku
+ */
+
+// Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/decima'));
+
+app.get('/*', function (req, res) {
+
+  res.sendFile(path.join(__dirname, '/dist/decima/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 5000);
